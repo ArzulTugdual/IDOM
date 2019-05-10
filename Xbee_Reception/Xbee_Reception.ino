5fb7de8f1887286d1708014556d047310b30ad1c
CHANCEL <louis.f.chancel@gmail.com>
2019-05-10 16:47:06 +0200

    XBee_COM

--- Xbee_Reception/Xbee_Reception.ino
+++ Xbee_Reception/Xbee_Reception.ino
@@ -1,20 +1,17 @@
 #include <SoftwareSerial.h>
 #include <XBee.h>
 
-int val;
 SoftwareSerial xbee(3,2);
 
 void setup(){
-            Serial.begin(9600);
-           }
+  xbee.begin(9600);
+  Serial.begin(9600);
+}
            
 void loop(){
-            int Byte;
-            if(Serial.available()){
-                                   int dataByte = Serial.read();
-                                   val = dataByte;
-                                   Serial.print(val,Byte);
-                                   }
-                                   
-
+  if(xbee.available()){
+    while(xbee.available())  {
+      Serial.write(xbee.read());
+    }
+  }
 }
--- Xbee_Transmission/Xbee_Transmission.ino
+++ Xbee_Transmission/Xbee_Transmission.ino
@@ -1,22 +1,20 @@
-#include <XBee.h>
 #include <SoftwareSerial.h>
+#include <XBee.h>
 
 SoftwareSerial xbee(2,3);
-char val;
-void setup() {
-              Serial.begin(9600);
-              xbee.begin(9600);
-              }
-void loop() {
-              if(Serial.available()){
-                                     val=Serial.read();
-                                     Serial.println(val);
-                                     xbee.print(val);
-                                     delay(150);
-                                     }
-             
-              else{
-                    Serial.write(0);
-                    }
-   
+int count;
+
+void setup(){
+  xbee.begin(9600);
+  Serial.begin(9600);
+  count=0;
+}
+
+void loop(){
+  String phrase = "Envoi du mot Sending";
+  phrase += count;
+  Serial.println(phrase);
+  xbee.println(phrase);
+  count++;
+  delay(300);
 }
