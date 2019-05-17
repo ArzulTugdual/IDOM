dbff43a914e198fad7fff90bf78c198647c876a2
Tugdual <tugdualarzul@gmail.com>
2019-05-17 15:45:02 +0200

    figures+boutons+capteur_ok
    tourne à droite de 90 degré quand il rencontre un obstacle

--- fonction_temps/fonction_temps.ino
+++ fonction_temps/fonction_temps.ino
@@ -23,23 +23,35 @@ MotorWheel wheel4(10,7,18,19,&irq4);
 
 Omni4WD Omni(&wheel1,&wheel2,&wheel3,&wheel4);
 
+/*variables de temps*/
 unsigned long seuilInit = 3000;
 unsigned long seuil = 0; //tps d'exécution d'un état
 unsigned long t0; //temps de départ d'un état
 unsigned long t;
+
 int figure=7; //carré sans rotation:0 carré avec rotation:1 triangle:2  ...
 int etatEnCours=0; //0:stop >0: différents états en fonction de la figure
 int vitessePWM = 40;  //vitesse de marche du robot
 
+/*capteur ultrason*/
+const byte PIN = 6;     // Broche capteur ultrason (envoie/réception) attention, pin 6 car non utilisé par le robot
+float distanceMin = 300; //distance min à un obstacle
 void setup()
 {
+  /*robot*/
   TCCR1B=TCCR1B&0xf8|0x01;    // Pin9,Pin10 PWM 31250Hz
   TCCR2B=TCCR2B&0xf8|0x01;    // Pin3,Pin11 PWM 31250Hz
 
   Omni.PIDEnable(0.31,0.01,0,10);
 
+  /*boutons*/
   pinMode(boutonS1_5,INPUT);  //pin correspondant aux boutons poussoirs S1 à S5
-  
+
+  /*capteur ultrason*/
+  pinMode(PIN, OUTPUT);
+  digitalWrite(PIN, LOW); // La broche doit être à LOW au repos
+
+  /*début du compteur*/
   t0 = millis();
 }
 
@@ -48,6 +60,12 @@ void loop()
   t = millis();
   int bouton = Bouton();
   bool chang_fig = bouton <=5;
+  if(mesure_distance()<distanceMin)
+  {
+    stopp();
+    delay(250);
+    td(90);
+  }
   if(t-t0>=seuil || chang_fig) //fin de l'état en cours -> changement d'état OU changement manuel de la figure
   {
     if(chang_fig)
@@ -69,10 +87,37 @@ void loop()
 
 /**
  * lit la valeur du pin des boutons poussoirs de la carte
+ * return 0:boutonS0 1:boutonS1 2:boutonS2 3:boutonS3 5:boutonS5
  */
 int Bouton()
 {
   return (analogRead(boutonS1_5)+66)/145;  //renvoie la valeur du bouton pressé
+}
+
+/**
+ * capteur de distance
+ * return : distance au prochain obstacle (en mm)
+ */
+ float mesure_distance()
+{
+  //capteur parallax (3 broches)
+  /*création des variables de résultat*/
+  float measure, distance_mm;
+  //float c = 20.05*sqrt(temp)*pow(10,-3);   //vitesse du son dans l'air en fonction de la température en mm/microsec
+  float c = 0.34;
+  
+  pinMode(PIN, OUTPUT);       //broche en sortie pour envoyer l'impulsion
+  digitalWrite(PIN, LOW);     // La broche doit être à LOW au départ
+  
+  /*impulsion de 10 micros*/
+  digitalWrite(PIN, HIGH);      
+  delayMicroseconds(10);
+  digitalWrite(PIN, LOW);       //envoi de l'onde sonore ___---___
+
+  pinMode(PIN, INPUT);          //broche en entrée en attente de l'impulsion
+  
+  measure = pulseIn(PIN, HIGH, 25000UL);  //attente du signal retour puis calcul du temps de réponse en microsec
+  return measure / 2.0 * c;               //MESURE en mm
 }
 
 /*figures*/
@@ -112,65 +157,67 @@ int carreAvecRotation(int etat)
   switch(etat)
   {
     case 0:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
-    break;
+      }
+      break;
     case 2:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
-    break;
+      }
+      break;
     case 4:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
-    break;
+      }
+      break;
     case 6:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
-    break;
+      }
+      break;
     case 1:
-    {
-    td(90);
-    seuil = 0;  //état terminé donc seuil à 0
-    } 
-    case 3:
-    {
-    td(90);
-    seuil = 0;  //état terminé donc seuil à 0
-    } 
-    case 5:
-    {
-    td(90);
-    seuil = 0;  //état terminé donc seuil à 0
-    } 
-    break;
-    case 7:
-    {
+      {
       td(90);
       seuil = 0;  //état terminé donc seuil à 0
-    } 
-    break;
+      }
+      break;
+    case 3:
+      {
+      td(90);
+      seuil = 0;  //état terminé donc seuil à 0
+      }
+      break; 
+    case 5:
+      {
+      td(90);
+      seuil = 0;  //état terminé donc seuil à 0
+      } 
+      break;
+    case 7:
+      {
+      td(90);
+      seuil = 0;  //état terminé donc seuil à 0
+      } 
+      break;
     default:
-    {
+      {
       stopp();
       seuil = 1000; //délais avec la prochaine figure
       figure = 10;  //empêche de touner la figure en boucle
-    }
-    break;
+      }
+      break;
   }
   etat = (etat+1)%9;
   return etat;
 } 
 
 /**
- * change l'état du robot (avant, tourner droite) en fonction de l'état suivant sur la figure "triangle"
+ * chawnge l'état du robot (avant, tourner droite) en fonction de l'état suivant sur la figure "triangle"
  * @param etat: état du robot (0:stop   impaire(1-5):avant    paire(2-6):tourner droite)
  */
 int triangle(int etat)
@@ -179,44 +226,48 @@ int triangle(int etat)
   switch(etat)
   {
     case 0:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
+      }
+      break;
     case 2:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
+      }
+      break;
     case 4:
-    {
+      {
       av(vitessePWM);
       seuil = seuilInit; //rétablissement de la durée d'un état
-    }
-    break;
+      }
+      break;
     case 1:
-    {
+      {
       td(120);
       seuil = 0;  //état terminé donc seuil à 0
-    } 
+      }
+      break;
     case 3:
-    {
+      {
       td(120);
       seuil = 0;  //état terminé donc seuil à 0
-    }
+      }
+      break;
     case 5:
-    {
+      {
       td(120);
       seuil = 0;  //état terminé donc seuil à 0
-    } 
-    break;
+      } 
+      break;
     default:
-    {
+      {
       stopp();
       seuil = 1000; //délais avec la prochaine figure
       figure = 10;  //empêche de touner la figure en boucle
-    }
-    break;
+      }
+      break;
   }
   etat = (etat+1)%7;
   return etat;
@@ -276,25 +327,25 @@ void g(int rapportPWM)
 }
 void td(int angle)
 {
-  wheel1.advancePWM(57);
-  wheel2.advancePWM(57);
-  wheel3.advancePWM(57);
-  wheel4.advancePWM(57);
+  wheel1.advancePWM(50);
+  wheel2.advancePWM(50);
+  wheel3.advancePWM(50);
+  wheel4.advancePWM(50);
   if(angle >= 0)
   {
-    delay((unsigned long)angle*(5013*1.0/360));
+    delay((unsigned long)angle*(5400*1.0/360));
     stopp();
   }
 }
 void tg(int angle)
 {
-  wheel1.backoffPWM(57);
-  wheel2.backoffPWM(57);
-  wheel3.backoffPWM(57);
-  wheel4.backoffPWM(57);
+  wheel1.backoffPWM(50);
+  wheel2.backoffPWM(50);
+  wheel3.backoffPWM(50);
+  wheel4.backoffPWM(50);
   if(angle >= 0)
   {
-    delay((unsigned long)angle*(5013*1.0/360));
+    delay((unsigned long)angle*(5400*1.0/360));
     stopp();
   }
 }
